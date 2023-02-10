b895c5408c48b4b5b99fd79f12b1820f4ae87450
hitesh Suthar <104884942+hitesh5444@users.noreply.github.com>
2023-02-10 22:01:44 +0530

    change 2

---
+++ assets/jquery/active.js
@@ -0,0 +1,25 @@
+var header = document.getElementById("myDIV");
+var btns = header.getElementsByClassName("nav-link");
+for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function () {
+        var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+    });
+}
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 10000); // Change image every 10 seconds
+}
+
